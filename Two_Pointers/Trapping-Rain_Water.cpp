18ffaaea4e3c5347177421db9c54e0a6a9c0c04d
Paths06 <91739556+Paths06@users.noreply.github.com>
2026-03-18 02:00:30 +0530

    Implement Trapping Rain Water algorithm

---
+++ Two_Pointers/Trapping-Rain_Water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int totalWater = 0;
+        int n = height.size();
+        int left = 0, right = n-1;
+        int leftMax = 0, rightMax =0;
+        while(left<right){
+            if(height[left]<height[right]){
+                leftMax = max(leftMax, height[left]);
+                totalWater+=(leftMax-height[left]);
+                left++;
+            }
+            else{
+                rightMax = max(rightMax, height[right]);
+                totalWater+=(rightMax-height[right]);
+                right--;
+            }
+        }
+        return totalWater;
+    }
+};
